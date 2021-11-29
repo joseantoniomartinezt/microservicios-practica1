6b30b2a39c6471801b49f2dcea7e780ea75a50a3
joseantonio <joseantoniomartinez1904@gmail.com>
2021-11-29 11:30:01 +0100

    first commit

---
+++ src/main/java/com/everis/practica1microservicios/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.everis.practica1microservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
